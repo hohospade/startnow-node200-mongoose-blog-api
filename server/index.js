462bf7a1b900c7d7592618c204457f9dd0b599ba
hohospade <vigne6@gmail.com>
2018-05-22 21:20:29 -0700

    travis11

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const server = require('./app');
-const PORT = process.env.PORT | |8080;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, function() {
   console.log('The EXPRESS server is listening on port 8080.');
